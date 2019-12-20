d28db8b7866ad7c72887fb1490661a5317b14589
James Sayer <james.sayer0908@gmail.com>
2019-12-20 10:37:47 -0800

    Firebase stuff

---
+++ src/Firebase/init.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+// import firestore from '/firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDqRa3O0YBOiJWIcmdBTyo5qhSYBrxf40E",
+    authDomain: "specialtystars.firebaseapp.com",
+    databaseURL: "https://specialtystars.firebaseio.com",
+    projectId: "specialtystars",
+    storageBucket: "specialtystars.appspot.com",
+    messagingSenderId: "876655856558",
+    appId: "1:876655856558:web:32bcb997948ca0df15563f",
+    measurementId: "G-P43CDSNW7Q"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  export default firebaseApp.firestore()
