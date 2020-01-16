ec08b9e3c1ce564f0357a3e7926a44206b5e100c
James Sayer <james.sayer0908@gmail.com>
2020-01-16 13:18:08 -0800

    Single Login

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,18 +1,39 @@
 <template>
-   <v-container class="loginCard">
-    <v-toolbar color="primary lighten-2" dark justify="center" max-width="450px" flat>
+  <v-container class="loginCard">
+    <v-toolbar
+      color="primary lighten-2"
+      dark
+      justify="center"
+      max-width="450px"
+      flat
+    >
       <v-row justify="space-around">
         <v-toolbar-title><h1>Signup Free</h1></v-toolbar-title>
       </v-row>
     </v-toolbar>
     <v-card>
-        <v-card-text>
+      <v-card-text>
         <v-form @submit.prevent="signup" class="card-panel">
-            
-            <v-text-field  type="Email" v-model="email" :rules="emailRules" label="E-mail:" required></v-text-field>
-            <v-text-field  type="password" v-model="password" label="Password:" required></v-text-field>
-            <v-text-field  type="alias" v-model="alias" label="Nick Name:" required></v-text-field>
-            <v-select
+          <v-text-field
+            type="Email"
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail:"
+            required
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            v-model="password"
+            label="Password:"
+            required
+          ></v-text-field>
+          <v-text-field
+            type="alias"
+            v-model="alias"
+            label="Nick Name:"
+            required
+          ></v-text-field>
+          <v-select
             v-model="role"
             v-bind:items="roles"
             label="Role:"
@@ -22,67 +43,67 @@
             required
           ></v-select>
 
-            <p class="red--text text-center" v-if="feedback">{{ feedback }}</p>
-            <v-card-actions class="text-center">
+          <p class="red--text text-center" v-if="feedback">{{ feedback }}</p>
+          <v-card-actions class="text-center">
             <div class="flex-grow-1"></div>
             <v-btn @click="signup" color="primary lighten-2"> Submit </v-btn>
-            </v-card-actions>
+          </v-card-actions>
         </v-form>
-        </v-card-text>
+      </v-card-text>
     </v-card>
-    </v-container>
+  </v-container>
 </template>
 
 <script>
-import db from '@/firebase/init'
-import slugify from 'slugify'
-import firebase from 'firebase'
+import db from "@/firebase/init";
+import slugify from "slugify";
+import firebase from "firebase";
 
 export default {
-  name: 'Signup',
-  data(){
-    return{
-      role: '',
-      roles: [
-        { text: 'Worker'},
-        { text: 'Employer'}
-      ],
+  name: "Signup",
+  data() {
+    return {
+      role: "",
+      roles: [{ text: "Worker" }, { text: "Employer" }],
       email: null,
       password: null,
       alias: null,
       feedback: null,
       slug: null,
+      myCred: null,
       emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       ]
     };
   },
-  
+
   methods: {
-    
-    signup(){
-      if(this.alias && this.email && this.password && this.role){
+    signup() {
+      if (this.alias && this.email && this.password && this.role) {
         this.slug = slugify(this.alias, {
-          replacement: '-',
+          replacement: "-",
           remove: /[$*_+~.()'"!\-:@]/g,
           lower: true
-        })
-        let ref = db.collection('users').doc(this.slug)
+        });
+        let ref = db.collection("users").doc(this.slug);
         ref.get().then(doc => {
-          if(doc.exists){
-            this.feedback = 'This alias already exists'
+          if (doc.exists) {
+            this.feedback = "This alias already exists";
           } else {
             // this alias does not yet exists in the db
-            firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(cred => {
-              db.collection('users').doc(cred.user.uid).set({
-                role: this.role
-              })
-              ref.set({
-                  alias: this.alias,
-                  geolocation: null,
-                  user_id: cred.user.uid
-               });
+            firebase
+              .auth()
+              .createUserWithEmailAndPassword(this.email, this.password)
+              .then(cred => {
+                this.myCred = cred
+                db.collection("users")
+                  .doc(cred.user.uid)
+                  .set({
+                    role: this.role,
+                    alias: this.alias,
+                    geolocation: null
+                  });
               })
               .then(() => {
                 this.myCred.user
@@ -106,11 +127,11 @@ export default {
           }
         });
       } else {
-        this.feedback = 'Please enter data in all fields'
+        this.feedback = "Please enter data in all fields";
       }
     }
   },
-   beforeCreate() {
+  beforeCreate() {
     firebase
       .auth()
       .signInAnonymously()
@@ -118,8 +139,10 @@ export default {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        alert('ErrorCode: ' + errorCode +'\n' + 'ErrorMessage: ' + errorMessage)
+        alert(
+          "ErrorCode: " + errorCode + "\n" + "ErrorMessage: " + errorMessage
+        );
       });
-  },
-}
+  }
+};
 </script>
